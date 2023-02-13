e1ef8dc3b9cd517b1ff12e4407ba9db322378719
7007singh <shw12021982@gmail.com>
2023-02-13 13:57:09 +0530

    [Shweta]Add: Find maximum integer number

---
+++ GenericConcept/FindMaximum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericConcept
+{
+    internal class FindMaximum
+    {
+        public static void FindMaximumValue(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"Maximum value is {first}");
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine($"Maximum value is {second}");
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                Console.WriteLine($"Maximum value is {third}");
+            }
+            else
+            {
+                Console.WriteLine("Two or three numbers are same");
+            }
+        }
+    }
+}
---
+++ GenericConcept/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first integer number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first integer number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter first integer number");
+            int third = Convert.ToInt32(Console.ReadLine());
+
+            FindMaximum.FindMaximumValue(first,second,third);
+            Console.ReadLine();
+        }
+    }
+}
